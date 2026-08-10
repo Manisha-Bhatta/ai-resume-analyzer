4c1e2172a64296694c8d7ffd17db463d0b2874fa
Manisha Bhatta <manishabhatta11@gmail.com>
2026-08-10 12:01:24 +0530

    Connect frontend to deployed backend

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*"
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://ai-resume-analyzer-three-cyan.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
